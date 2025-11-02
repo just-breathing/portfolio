fbbb8b9969214ac4a2a23794d2f7fe088f0228df
just-breathing <sundeep.reddy.n.2000@gmail.com>
2025-11-02 01:38:50 -0400

    fixed build issue

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
 import { AnimatedString } from "./home";
 import ThemeToggle from "./themeToggle";
